c406c71edd1cba6bcf3c2248627abec9a8449f4f
Student <Student@205-09>
2023-10-17 15:56:46 +0800

    加入專案檔案。

---
+++ KidsLearningApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KidsLearningApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KidsLearningApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace KidsLearningApp
+{
+    public partial class MainPage : ContentPage
+    {
+    
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
